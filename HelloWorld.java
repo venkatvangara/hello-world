c8abb1a2c0196eaf78e437b3c6039af8c5cc9e12
Vangara Venkat <39134961+venkatvangara@users.noreply.github.com>
2020-10-07 14:19:57 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.sanumala;
+public class HelloWorld
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello gist @ GitHub!");   
+      }
+} 
+
